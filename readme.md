08a333d911a390d3b797494fbdd3135fe655a035
Charles Louw <charles.louw@gmail.com>
2019-01-22 18:08:04 +0200

    feat: update code fields to match iso codes

---
+++ readme.md
@@ -0,0 +1,7 @@
+# Laravel Languages
+
+Migrate & populate table of langauges as per ISO 639-1 & ISO 639-3
+
+## Roadmap
+
+- Update the ISO 639-3 code; since most of them are still just copied from the ISO 639-1 code
--- src/Model/Language.php
+++ src/Model/Language.php
@@ -5,4 +5,12 @@ namespace Cubitworx\Laravel\Languages\Model;
 use Illuminate\Database\Eloquent\Model;
 
 class Language extends Model {
+
+	protected $guarded = [
+		'created_at',
+		'created_by',
+		'updated_at',
+		'updated_by',
+	];
+
 }
