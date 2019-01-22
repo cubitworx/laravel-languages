2d39d97a1c6aa255033318c4667c70e7162e4ee4
Charles Louw <charles.louw@gmail.com>
2019-01-22 14:29:29 +0200

    feat: initial commit

---
+++ src/Model/Language.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Cubitworx\Laravel\Languages\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Language extends Model {
+}
---
+++ src/Providers/ServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Cubitworx\Laravel\Languages\Providers;
+
+use Illuminate\Support\ServiceProvider as BaseServiceProvider;
+
+class ServiceProvider extends BaseServiceProvider {
+
+	/**
+	 * Bootstrap services.
+	 *
+	 * @return void
+	 */
+	public function boot() {
+		$this->loadMigrationsFrom(__DIR__.'/../migrations');
+	}
+
+	/**
+	 * Register services.
+	 *
+	 * @return void
+	 */
+	public function register() {
+		//
+	}
+
+}
